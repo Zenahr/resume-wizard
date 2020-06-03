c74f386f42205e1dc20fb488784ae0f8cac03c7d
Zenahr Barzani <zenmatica@gmail.com>
2020-06-03 23:36:06 +0200

    integrated resume template

--- script.py
+++ script.py
@@ -7,12 +7,19 @@ template = template_env.get_template('template.html')
 
 with open('template/config.yaml', 'r') as config_file:
     config = yaml.load(config_file)
-    config = config['content']
 
 with open('public/index.html', 'w') as output_file:
     output_file.write(
         template.render(
-            firstName=config['firstName'],
-            lastName=config['lastName']
+            title=config['content']['title'],
+            greetingText=config['content']['greetingText'],
+            resumeText=config['content']['resumeText'],
+            firstName=config['title']['firstName'],
+            lastName=config['title']['lastName'],
+            github=config['socials']['github'],
+            linkedIn=config['socials']['linkedIn'],
+            email=config['socials']['email'],
+            closingStatement=config['content']['closingStatement'],
+            js=""
         )
     )
