733c26c64e6c540f99a86419539f0066e098f049
Zenahr Barzani <zenmatica@gmail.com>
2020-06-03 23:12:11 +0200

    added yaml support

--- script.py
+++ script.py
@@ -1,20 +1,18 @@
-from markdown2 import markdown
 from jinja2 import Environment, FileSystemLoader
 from json import load
+import yaml
 
 template_env = Environment(loader=FileSystemLoader(searchpath='./template'))
 template = template_env.get_template('template.html')
 
-with open('content.md') as markdown_file:
-    article = markdown(markdown_file.read())
-
-with open('template/content.json') as config_file:
-    config = load(config_file)
+with open('template/config.yaml', 'r') as config_file:
+    config = yaml.load(config_file)
+    config = config['content']
 
 with open('public/index.html', 'w') as output_file:
     output_file.write(
         template.render(
-            title=config['title'],
-            article=config['article']
+            firstName=config['firstName'],
+            lastName=config['lastName']
         )
     )
