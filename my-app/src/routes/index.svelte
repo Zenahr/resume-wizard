eb72bacaa86d2cdd93dbaac5040034668a705433
Zenahr Barzani <zenmatica@gmail.com>
2020-06-03 20:23:47 +0200

    removed sapper again

--- my-app/src/routes/index.svelte
+++ my-app/src/routes/index.svelte
@@ -40,7 +40,7 @@
 
 <figure>
 	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
+	<figcaption>Have fun with Sapper</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
